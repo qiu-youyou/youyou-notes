63c1ed7d1bfc81b09e1dec59c40e907736971c16
s_qiu <s.qiu@foxmail.com>
2025-04-19 10:09:45 +0800

    feat: algorithm math palindrome-number

--- docs/.vitepress/config/sidebar/sidebar-algorithm.ts
+++ docs/.vitepress/config/sidebar/sidebar-algorithm.ts
@@ -1,18 +1,13 @@
-import { DefaultTheme } from 'vitepress';
+import { DefaultTheme } from "vitepress";
 
 export const sidebarAlgorithm: DefaultTheme.SidebarItem[] = [
   {
-    text: 'waiting',
-    link: '/algorithm/',
-    collapsed: false,
-  },
-  {
-    text: '数学',
+    text: "数学",
     collapsed: false,
     items: [
       {
-        text: '回文数',
-        link: '/algorithm/math/palindrome-number/',
+        text: "回文数",
+        link: "/algorithm/math/palindrome-number/",
       },
     ],
   },
