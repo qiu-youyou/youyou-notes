49053396ec6d15508b6d0091f86f9a25d5d4c0e8
s_qiu <s.qiu@foxmail.com>
2025-04-28 11:38:15 +0800

    feat: node-server-started

--- docs/.vitepress/config/sidebar/sidebar-blog.ts
+++ docs/.vitepress/config/sidebar/sidebar-blog.ts
@@ -1,67 +1,67 @@
-import { DefaultTheme } from "vitepress";
+import { DefaultTheme } from 'vitepress';
 
 export const sidebarBlog: DefaultTheme.SidebarItem[] = [
   {
-    text: "Vue3 基础项目模板搭建",
+    text: 'Vue3 基础项目模板搭建',
     collapsed: false,
     items: [
       {
-        text: "初始化 Vue3 项目",
-        link: "/blog/vue3-template/init-project/",
+        text: '初始化 Vue3 项目',
+        link: '/blog/vue3-template/init-project/',
       },
       {
-        text: "渐进式Web应用- PWA",
-        link: "/blog/vue3-template/pwa/",
+        text: '渐进式Web应用- PWA',
+        link: '/blog/vue3-template/pwa/',
       },
       {
-        text: "单元测试及端到端测试配置",
-        link: "/blog/vue3-template/unit-e2e-test/",
+        text: '单元测试及端到端测试配置',
+        link: '/blog/vue3-template/unit-e2e-test/',
       },
     ],
   },
 
   {
-    text: "TypeScript 语法用法",
+    text: 'TypeScript 语法用法',
     collapsed: false,
     items: [
       {
-        text: "TypeScript-相关概念",
-        link: "/blog/typescript-started/typescript-concept",
+        text: 'TypeScript-相关概念',
+        link: '/blog/typescript-started/typescript-concept',
       },
       {
-        text: "TypeScript-基本类型",
-        link: "/blog/typescript-started/typescript-type",
+        text: 'TypeScript-基本类型',
+        link: '/blog/typescript-started/typescript-type',
       },
       {
-        text: "TypeScript-函数类型",
-        link: "/blog/typescript-started/typescript-fun",
+        text: 'TypeScript-函数类型',
+        link: '/blog/typescript-started/typescript-fun',
       },
       {
-        text: "TypeScript-特殊类型",
-        link: "/blog/typescript-started/typescript-special",
+        text: 'TypeScript-特殊类型',
+        link: '/blog/typescript-started/typescript-special',
       },
       {
-        text: "TypeScript-Class类",
-        link: "/blog/typescript-started/typescript-class",
+        text: 'TypeScript-Class类',
+        link: '/blog/typescript-started/typescript-class',
       },
       {
-        text: "TypeScript-Generics泛型",
-        link: "/blog/typescript-started/typescript-generics",
+        text: 'TypeScript-Generics泛型',
+        link: '/blog/typescript-started/typescript-generics',
       },
       {
-        text: "TypeScript-Interface接口",
-        link: "/blog/typescript-started/typescript-interface",
+        text: 'TypeScript-Interface接口',
+        link: '/blog/typescript-started/typescript-interface',
       },
     ],
   },
 
   {
-    text: "JavaScript 设计模式",
+    text: 'JavaScript 设计模式',
     collapsed: false,
     items: [
       {
-        text: "JavaScript 设计模式-面向对象",
-        link: "/blog/javascript-patterns/oop/",
+        text: 'JavaScript 设计模式-面向对象',
+        link: '/blog/javascript-patterns/oop/',
       },
       // {
       //   text: "JavaScript设计模式-设计原则",
@@ -75,24 +75,28 @@ export const sidebarBlog: DefaultTheme.SidebarItem[] = [
   },
 
   {
-    text: "快速上手系列",
+    text: '快速上手系列',
     collapsed: false,
     items: [
       {
-        text: "React-Router(V6.4)-快速上手",
-        link: "/blog/get-started/react-router/",
+        text: '上手Node.js与数据库服务端开发',
+        link: '/blog/get-started/node-server-started/',
       },
       {
-        text: "Redux&Redux Toolkit-快速上手",
-        link: "/blog/get-started/redux-tootik/",
+        text: 'React-Router(V6.4)-快速上手',
+        link: '/blog/get-started/react-router/',
       },
       {
-        text: "create-react-app 初始化项目",
-        link: "/blog/get-started/create-react-app/",
+        text: 'Redux&Redux Toolkit-快速上手',
+        link: '/blog/get-started/redux-tootik/',
       },
       {
-        text: "React-快速上手",
-        link: "/blog/get-started/react/",
+        text: 'create-react-app 初始化项目',
+        link: '/blog/get-started/create-react-app/',
+      },
+      {
+        text: 'React-快速上手',
+        link: '/blog/get-started/react/',
       },
     ],
   },
