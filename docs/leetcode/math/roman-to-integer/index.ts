9e5a4476b5e06e450e78e45d26bbb6046da50a36
s_qiu <s.qiu@foxmail.com>
2025-04-30 11:39:12 +0800

    feat: roman-to-integer

--- docs/leetcode/math/roman-to-integer/index.ts
+++ docs/leetcode/math/roman-to-integer/index.ts
@@ -5,8 +5,36 @@
  */
 
 // @lc code=start
-function romanToInt(s: string): number {
-  return 1
+const romanMap: Record<string, number> = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
 };
-// @lc code=end
 
+// 特殊的
+const spRomanMap: Record<string, number> = {
+  IV: 4,
+  IX: 9,
+  XL: 40,
+  XC: 90,
+  CD: 400,
+  CM: 900,
+};
+
+function romanToInt(s: string): number {
+  // 处理特殊的罗马数字
+  if (Object.keys(spRomanMap).indexOf(s) !== -1) {
+    return spRomanMap[s];
+  }
+  let result = 0;
+  s.split('').forEach((item) => {
+    result += romanMap[item];
+  });
+  return result;
+}
+
+// @lc code=end
